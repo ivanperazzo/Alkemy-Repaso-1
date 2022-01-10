52825bed434f8bde070cbd149ec5c264e63dd212
Ivan Perazzo <iperazzo@hotmail.com.ar>
2022-01-10 19:57:30 -0300

    Add project files.

---
+++ Repaso-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repaso_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
