493c65a4a36664b9b92193d9a5670b4e9d149387
Ivan Perazzo <iperazzo@hotmail.com.ar>
2022-01-10 20:12:48 -0300

    Add models

---
+++ Repaso-1/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso_1.Models
+{
+    class Comments
+    {
+        public int CommentId { get; set; }
+        public DateTime Date { get; set; }
+        public string Comment { get; set; }
+        public User User { get; set; }
+    }
+}
