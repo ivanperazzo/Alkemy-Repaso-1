ac8c552c71c8df90735242218f5aad0da3882294
Ivan Perazzo <iperazzo@hotmail.com.ar>
2022-01-10 20:14:48 -0300

    Add Alkemy DbContext

---
+++ Repaso-1/AlkemyDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Repaso_1.Models;
+
+namespace Repaso_1
+{
+    class AlkemyDbContext : DbContext
+    {
+        public AlkemyDbContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Posts> Posts { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+    }
+}
